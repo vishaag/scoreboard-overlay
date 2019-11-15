45cc6950ef227ee7b4b17d0fd88a4b51d7fc2a7c
vishaag <vishaag@gmail.com>
2019-11-16 06:59:28 +0800

    split timelines, add animation sequences, add title & first graphic

--- src/Overlays.js
+++ src/Overlays.js
@@ -3,21 +3,22 @@ import Scoreboard from "./Scoreboard";
 import MatchLogo from "./MatchLogo";
 
 class Overlays extends React.Component {
-  constructor({ timeline }) {
-    super();
-    this.timeline = timeline;
+  constructor({ scoreboardTimeline, logoTimeline }) {
+    super({ scoreboardTimeline, logoTimeline });
+    this.scoreboardTimeline = scoreboardTimeline;
+    this.logoTimeline = logoTimeline;
   }
   render() {
     const finalScore = "1-0";
-    const animationSequence = [{ animation: "main", delay: 0 }];
+    const scoreboardAnimationSequence = [{ animation: "main", delay: 1 }];
     return (
       <div className="overlays">
         <Scoreboard
           score={finalScore}
-          animation={animationSequence}
-          timeline={this.timeline}
+          animationSequence={scoreboardAnimationSequence}
+          timeline={this.scoreboardTimeline}
         />
-        <MatchLogo timeline={this.timeline} />
+        <MatchLogo timeline={this.logoTimeline} />
       </div>
     );
   }
--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,28 +1,54 @@
 import React from "react";
 
+import { Quad } from "gsap";
 class Scoreboard extends React.Component {
   constructor(props) {
     super(props);
 
     this.score = props.score;
-    this.animationSequnce = props.animationSequnce;
+    this.animationSequence = props.animationSequence;
     this.timeline = props.timeline;
 
     this.leftTeam = null;
     this.rightTeam = null;
     this.leftTeamText = null;
     this.rightTeamText = null;
-    this.score = null;
+    this.scoreBox = null;
     this.scoreText = null;
     this.time = null;
     this.timeText = null;
   }
 
   componentDidMount() {
-    this.timeline
-      // .to(this.leftTeam, 0.5, { x: -50, opacity: 0 }, "+=0.5")
-      .from(this.leftTeam, 1, { opacity: 0, x: "50px" })
-      .from(this.rightTeam, 1, { opacity: 0, x: "-100px" }, "=-1");
+    let states = [];
+    let animationDelayObject = {};
+    this.animationSequence.forEach(animationObject => {
+      states.push(animationObject.animation);
+      animationDelayObject[
+        animationObject.animation
+      ] = animationObject.delay.toString();
+    });
+
+    if (states.includes("main") && states.includes("teamStat")) {
+      this.timeline.from(
+        this.scoreBox,
+        1,
+        { opacity: 0, x: "-100px" },
+        `=${animationDelayObject["main"]}`
+      );
+    } else if (states.includes("main")) {
+      this.timeline.from(this.scoreText, 1, {
+        scaleX: 2,
+        scaleY: 2,
+        opacity: 0,
+        transformOrigin: "50% 50%"
+      });
+    }
+
+    // this.timeline
+    // .to(this.leftTeam, 0.5, { x: -50, opacity: 0 }, "+=0.5")
+    // .from(this.leftTeam, 1, { opacity: 0, x: "50px" })
+    // .from(this.rightTeam, 1, { opacity: 0, x: "-100px" }, "=-1");
     // .set(this.content, { autoAlpha: 1 }) // show content div
     // .from(this.leftTeam, 0.5, { left: 200, autoAlpha: 0 });
     // .from(this.rightTeam, 2.5, { left: -100, autoAlpha: 0 }); // added -0.25 seconds prior to end this.of timeline
@@ -35,36 +61,24 @@ class Scoreboard extends React.Component {
     return (
       <React.Fragment>
         <svg className="SVG" version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <g ref={g => (this.leftTeam = g)}>
+          <g>
             <rect
-              x="10"
-              y="10"
-              width="40"
+              ref={rect => (this.scoreBox = rect)}
+              x="100"
+              y="5"
+              width="75"
               height="40"
-              stroke="blue"
-              fill="blue"
-              strokeWidth="5"
+              fill="#00003f"
             />
             <text
-              x="17.5"
+              ref={text => (this.scoreText = text)}
+              x="120"
               y="32.5"
-              fontFamily="Verdana"
-              fontSize="10"
-              fill="white"
+              className="score-text"
             >
-              15
+              0-4
             </text>
           </g>
-          <rect
-            ref={rect => (this.rightTeam = rect)}
-            x="50"
-            y="10"
-            width="40"
-            height="40"
-            stroke="white"
-            fill="transparent"
-            strokeWidth="5"
-          />
         </svg>
       </React.Fragment>
     );
